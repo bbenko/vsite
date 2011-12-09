40190b7aabb4c8c9d9b7467959de679dc2d6f582
Bozidar Benko <bb@securityheroes.com>
2011-12-09 10:52:27 +0100

    SqlConnection, SqlCommand, ExecuteReader, ExecuteScalar, ExecuteNonQuery

---
+++ pred10/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtStudenti" runat="server" TextMode="MultiLine"></asp:TextBox>
+    </div>
+    <br />
+    <asp:TextBox ID="txtIme" runat="server"></asp:TextBox>
+    <asp:TextBox ID="txtPrezime" runat="server"></asp:TextBox>
+    <asp:TextBox ID="txtPbr" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+
+    </form>
+</body>
+</html>
---
+++ pred10/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlServerCe;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string connString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["FaksConnectionString"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {                                                                                           
+        SqlCeConnection conn = new SqlCeConnection(connString); // SqlConnection
+        conn.Open();
+        SqlCeCommand command = new SqlCeCommand("SELECT * FROM student WHERE pbr=@pbr", conn); // ... WHERE pbr=" + pbr.ToString() NE - SQL Injection
+        command.Parameters.AddWithValue("pbr", 10000); // ovako rjestiti i labose sa usernmom i passwordom
+
+        SqlCeDataReader dr = command.ExecuteReader();
+
+        //za labose: dr.HasRows
+        txtStudenti.Text = "";
+        while (dr.Read())
+        {
+            txtStudenti.Text += dr[0] + " " +  dr["ime"] + " " + dr["prezime"] + " "+ dr["pbr"] + "\n";
+        }
+
+        dr.Close();
+        command = new SqlCeCommand("SELECT COUNT(*) FROM student", conn);
+        int broj = (int)command.ExecuteScalar();
+        txtStudenti.Text += broj.ToString();
+        conn.Close();
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCeConnection conn = new SqlCeConnection(connString);
+        conn.Open();
+        SqlCeCommand command = new SqlCeCommand("INSERT INTO student(ime, prezime, pbr) VALUES (@ime, @prezime, @pbr)", conn);
+        command.Parameters.AddWithValue("ime", txtIme.Text);
+        command.Parameters.AddWithValue("prezime", txtPrezime.Text);
+        command.Parameters.AddWithValue("pbr", txtPbr.Text);
+
+        command.ExecuteNonQuery();
+        conn.Close();
+    }
+}
